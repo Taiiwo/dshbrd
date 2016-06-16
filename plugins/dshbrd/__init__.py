19ee6c40c54a365ec00c8589d0c1be39988fa148
Taiiwo Llort <Taiiwo@Llort.gq>
2016-06-16 09:20:58 +0100

    rename hackpaml -> dshbrd

---
+++ plugins/dshbrd/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from taiicms import app, config, save_config, plugins
+from taiicms.api import make_error_response, make_success_response, user
+
+@app.route('/api/plugin/hdshbrd/get-cards', methods=["POST"])
+def get_cards():
+    dir_names = []
+    for root, dirs, files in os.walk('plugins/dshbrd/cards/'):
+        for dir_name in dirs:
+            dir_names.append(dir_name)
+    return make_success_response({'cards': dir_names})
+
+def main(conf):
+    pass
